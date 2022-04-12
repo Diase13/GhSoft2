85bba9fb884ac225a2eeb4e390e9ab57215a2913
Diase13 <diego.alejandro.13@hotmail.com>
2022-04-12 14:00:27 -0500

    Estrucuta 1.0

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ghwebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
